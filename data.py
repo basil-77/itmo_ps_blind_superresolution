23d63e7f663a75b5cb0787ff1a1884fef0743627
basil-77 <basillavr@gmail.com>
2024-01-07 17:50:54 +0300

    data utils, v3 conv attentions, v4 swin-t

---
+++ data.py
@@ -0,0 +1,358 @@
+import os
+
+import numpy as np
+import random
+
+import torch
+import torchvision
+from torchvision.io import read_image
+from torchvision.transforms import v2
+
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+
+from tqdm import tqdm
+
+
+SEED = 42
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+class image_dataset_from_file(Dataset):
+    def __init__(self, source_path, target_path, size=None, normalize=False, source_mask=False, target_mask=False):
+        self.source_path = source_path
+        self.target_path = target_path
+        self.size = size
+        self.normalize = normalize
+        self.source_mask = source_mask
+        self.target_mask = target_mask
+        self.images_source = []
+        self.images_target = []
+                
+        for filename in tqdm(os.listdir(self.source_path)):
+            if self.source_mask:
+                if filename.find(self.source_mask)>0:
+                    self.images_source.append(os.path.join(self.source_path, filename))
+            else:
+                self.images_source.append(os.path.join(self.source_path, filename))
+
+        for filename in tqdm(os.listdir(self.target_path)):
+            if self.target_mask:
+                if filename.find(self.target_mask)>0:
+                    self.images_target.append(os.path.join(self.target_path, filename))
+            else:
+                self.images_target.append(os.path.join(self.target_path, filename))
+        
+        image_transforms = []
+        image_transforms.append(torchvision.transforms.v2.ToImage())
+        image_transforms.append(torchvision.transforms.v2.ToDtype(torch.float32, scale=True))
+        if self.size:
+            image_transforms.append(torchvision.transforms.Resize(size,
+                                                                  interpolation=torchvision.transforms.InterpolationMode.BILINEAR,
+                                                                  antialias=True))
+        self.transform = torchvision.transforms.Compose(image_transforms)
+        
+    def __len__(self):
+        return len(self.images_source)
+    
+    def __getitem__(self, index):
+        img = read_image(self.images_source[index])
+        img = self.transform(img)
+        data_source = img
+        img = read_image(self.images_target[index])
+        img = self.transform(img)
+        data_target = img
+        return data_source, data_target
+        
+
+class image_dataset_patches_from_file(Dataset):
+    def __init__(self, source_path, target_path, ref_path, patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        self.source_path = source_path
+        self.target_path = target_path
+        self.ref_path = ref_path
+        self.patch_size = patch_size
+        self.ref_scale = ref_scale
+        self.normalize = normalize
+        self.source_mask = source_mask
+        self.target_mask = target_mask
+        self.ref_mask = ref_mask
+        self.images_source = []
+        self.images_target = []
+        self.images_ref = []
+        self.images_source_img = []
+        self.images_target_img = []
+        self.images_ref_img = []
+        self.images_source_patches = []
+        self.images_target_patches = []
+        self.images_ref_patches = []
+        
+                
+        for filename in os.listdir(self.source_path):
+            if self.source_mask:
+                if filename.find(self.source_mask)>0:
+                    self.images_source.append(os.path.join(self.source_path, filename))
+            else:
+                self.images_source.append(os.path.join(self.source_path, filename))
+
+        for filename in os.listdir(self.target_path):
+            if self.target_mask:
+                if filename.find(self.target_mask)>0:
+                    self.images_target.append(os.path.join(self.target_path, filename))
+            else:
+                self.images_target.append(os.path.join(self.target_path, filename))
+
+        for filename in os.listdir(self.ref_path):
+            if self.ref_mask:
+                if filename.find(self.ref_mask)>0:
+                    self.images_ref.append(os.path.join(self.ref_path, filename))
+            else:
+                self.images_ref.append(os.path.join(self.ref_path, filename))
+                
+                
+        image_transforms = []
+        image_transforms.append(torchvision.transforms.v2.ToImage())
+        image_transforms.append(torchvision.transforms.v2.ToDtype(torch.float32, scale=True))
+        self.transform = torchvision.transforms.Compose(image_transforms)
+        
+        for image in tqdm(self.images_source):
+            img = read_image(image)
+            if img.shape[0]!= 3:
+                img = img.expand(3,-1,-1)
+            if (img.shape[1]>=patch_size[0] and img.shape[2]>=patch_size[1]):
+                img_tr = self.transform(img)
+                self.images_source_img.append(img_tr)
+                patches = img_tr.data.unfold(0, 3, 3).unfold(1, patch_size[0], patch_size[1]).unfold(2, patch_size[0], patch_size[1])
+                size = patches[0].shape
+                for i in range(size[0]):
+                    for j in range(size[1]):
+                        self.images_source_patches.append(patches[0][i][j])
+            else:
+                self.images_source.remove(image)
+                
+        for image in tqdm(self.images_target):
+            img = read_image(image)
+            if img.shape[0]!= 3:
+                img = img.expand(3,-1,-1)
+            if (img.shape[1]>=patch_size[0] and img.shape[2]>=patch_size[1]):
+                img_tr = self.transform(img)
+                self.images_target_img.append(img_tr)
+                patches = img_tr.data.unfold(0, 3, 3).unfold(1, patch_size[0], patch_size[1]).unfold(2, patch_size[0], patch_size[1])
+                size = patches[0].shape
+                for i in range(size[0]):
+                    for j in range(size[1]):
+                        self.images_target_patches.append(patches[0][i][j])
+            else:
+                self.images_target.remove(image)
+                
+        for image in tqdm(self.images_ref):
+            img = read_image(image)
+            if img.shape[0]!= 3:
+                img = img.expand(3,-1,-1)
+            if (img.shape[1]>=patch_size[0]*self.ref_scale and img.shape[2]>=patch_size[1]*self.ref_scale):    
+                img_tr = self.transform(img)
+                self.images_ref_img.append(img_tr)
+                patches = img_tr.data.unfold(0, 3, 3).unfold(1,
+                                                             patch_size[0]*self.ref_scale,
+                                                             patch_size[1]*self.ref_scale).unfold(2,
+                                                                                                  patch_size[0]*self.ref_scale,
+                                                                                                  patch_size[1]*self.ref_scale)
+                size = patches[0].shape
+                for i in range(size[0]):
+                    for j in range(size[1]):
+                        self.images_ref_patches.append(patches[0][i][j])
+            else:
+                self.images_ref.remove(image)
+
+                
+    def __len__(self):
+        return len(self.images_source)
+    
+    def __getitem__(self, index):
+        data_source = self.images_source_patches[index]
+        data_target = self.images_target_patches[index]
+        return data_source, data_target
+
+class data_realsr_canon_x2_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/RealSR(V3)/canon/train/2',
+                 target_path = './data/RealSR(V3)/canon/train/2',
+                 ref_path = './data/RealSR(V3)/canon/train/2',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask='LR', target_mask='LR', ref_mask='HR'):
+        super(data_realsr_canon_x2_train, self).__init__(source_path, target_path, ref_path, source_mask=source_mask, target_mask=target_mask, ref_mask=ref_mask)
+
+class data_realsr_canon_x3_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/RealSR(V3)/canon/train/3',
+                 target_path = './data/RealSR(V3)/canon/train/3',
+                 ref_path = './data/RealSR(V3)/canon/train/3',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask='LR', target_mask='LR', ref_mask='HR'):
+        super(data_realsr_canon_x3_train, self).__init__(source_path, target_path, ref_path, source_mask=source_mask, target_mask=target_mask, ref_mask=ref_mask)
+
+class data_realsr_canon_x4_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/RealSR(V3)/canon/train/4',
+                 target_path = './data/RealSR(V3)/canon/train/4',
+                 ref_path = './data/RealSR(V3)/canon/train/4',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask='LR', target_mask='LR', ref_mask='HR'):
+        super(data_realsr_canon_x4_train, self).__init__(source_path, target_path, source_mask=source_mask, target_mask=target_mask, ref_mask=ref_mask)        
+
+class data_imagenet(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/imagenet/train',
+                 target_path = './data/imagenet/train',
+                 ref_path = './data/imagenet/train',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_imagenet, self).__init__(source_path, target_path, ref_path)
+
+
+class data_div2k_x2_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/div2k/DIV2K_train_LR_unknown/X2',
+                 target_path = './data/div2k/DIV2K_train_LR_unknown/X2',
+                 ref_path = './data/div2k/DIV2K_train_HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_div2k_x2_train, self).__init__(source_path, target_path, ref_path)
+        
+class data_div2k_x3_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/div2k/DIV2K_train_LR_unknown/X3',
+                 target_path = './data/div2k/DIV2K_train_LR_unknown/X3',
+                 ref_path = './data/div2k/DIV2K_train_HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_div2k_x3_train, self).__init__(source_path, target_path, ref_path)
+        
+class data_div2k_x4_train(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/div2k/DIV2K_train_LR_unknown/X4',
+                 target_path = './data/div2k/DIV2K_train_LR_unknown/X4',
+                 ref_path = './data/div2k/DIV2K_train_HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_div2k_x4_train, self).__init__(source_path, target_path, ref_path)        
+        
+class data_bsd100_x2(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/B100/LR_bicubic/X2',
+                 target_path = './data/benchmark/B100/LR_bicubic/X2',
+                 ref_path = './data/benchmark/B100/HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_bsd100_x2, self).__init__(source_path, target_path, ref_path)
+
+class data_bsd100_x3(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/B100/LR_bicubic/X3',
+                 target_path = './data/benchmark/B100/LR_bicubic/X3',
+                 ref_path = './data/benchmark/B100/HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_bsd100_x3, self).__init__(source_path, target_path, ref_path)
+
+class data_bsd100_x4(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/B100/LR_bicubic/X4',
+                 target_path = './data/benchmark/B100/LR_bicubic/X4',
+                 ref_path = './data/benchmark/B100/HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_bsd100_x4, self).__init__(source_path, target_path, ref_path)
+
+
+class data_manga109_x2(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Manga109/LR_bicubic/X2',
+                 target_path = './data/benchmark/Manga109/LR_bicubic/X2',
+                 ref_path = './data/benchmark/Manga109/HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_manga109_x2, self).__init__(source_path, target_path, ref_path)        
+
+class data_manga109_x3(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Manga109/LR_bicubic/X3',
+                 target_path = './data/benchmark/Manga109/LR_bicubic/X3',
+                 ref_path = './data/benchmark/Manga109/HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_manga109_x3, self).__init__(source_path, target_path, ref_path)        
+
+class data_manga109_x4(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Manga109/LR_bicubic/X4',
+                 target_path = './data/benchmark/Manga109/LR_bicubic/X4',
+                 ref_path = './data/benchmark/Manga109/HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_manga109_x4, self).__init__(source_path, target_path, ref_path)        
+        
+class data_set5_x2(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set5/LR_bicubic/X2',
+                 target_path = './data/benchmark/Set5/LR_bicubic/X2',
+                 ref_path = './data/benchmark/Set5/HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set5_x2, self).__init__(source_path, target_path, ref_path)        
+
+class data_set5_x3(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set5/LR_bicubic/X3',
+                 target_path = './data/benchmark/Set5/LR_bicubic/X3',
+                 ref_path = './data/benchmark/Set5/HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set5_x3, self).__init__(source_path, target_path, ref_path)        
+
+class data_set5_x4(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set5/LR_bicubic/X4',
+                 target_path = './data/benchmark/Set5/LR_bicubic/X4',
+                 ref_path = './data/benchmark/Set5/HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set5_x4, self).__init__(source_path, target_path, ref_path)        
+
+        
+class data_set14_x2(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set14/LR_bicubic/X2',
+                 target_path = './data/benchmark/Set14/LR_bicubic/X2',
+                 ref_path = './data/benchmark/Set14/HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set14_x2, self).__init__(source_path, target_path, ref_path)
+
+class data_set14_x3(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set14/LR_bicubic/X3',
+                 target_path = './data/benchmark/Set14/LR_bicubic/X3',
+                 ref_path = './data/benchmark/Set14/HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set14_x3, self).__init__(source_path, target_path, ref_path)        
+
+class data_set14_x4(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Set14/LR_bicubic/X4',
+                 target_path = './data/benchmark/Set14/LR_bicubic/X4',
+                 ref_path = './data/benchmark/Set14/HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_set14_x4, self).__init__(source_path, target_path, ref_path)        
+
+class data_urban100_x2(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Urban100/LR_bicubic/X2',
+                 target_path = './data/benchmark/Urban100/LR_bicubic/X2',
+                 ref_path = './data/benchmark/Urban100/HR',
+                 patch_size=(256,256), ref_scale=2, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_urban100_x2, self).__init__(source_path, target_path, ref_path)
+
+class data_urban100_x3(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Urban100/LR_bicubic/X3',
+                 target_path = './data/benchmark/Urban100/LR_bicubic/X3',
+                 ref_path = './data/benchmark/Urban100/HR',
+                 patch_size=(256,256), ref_scale=3, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_urban100_x3, self).__init__(source_path, target_path, ref_path)        
+
+class data_urban100_x4(image_dataset_patches_from_file):
+    def __init__(self,
+                 source_path = './data/benchmark/Urban100/LR_bicubic/X4',
+                 target_path = './data/benchmark/Urban100/LR_bicubic/X4',
+                 ref_path = './data/benchmark/Urban100/HR',
+                 patch_size=(256,256), ref_scale=4, normalize=False, source_mask=False, target_mask=False, ref_mask=False):
+        super(data_urban100_x4, self).__init__(source_path, target_path, ref_path)        
+        
---
+++ torch_trainer.py
@@ -0,0 +1,170 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+import wandb
+from wandb.keras import WandbCallback
+
+import time
+
+import matplotlib.pyplot as plt
+
+
+class torch_trainer():
+
+    def __init__(self, model, criterion, optimizer, epochs, dataloader, device='cuda', scheduler=None):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.epochs = epochs
+        self.dataloader = dataloader
+        self.device = device
+        self.history = train_history()
+        self.metrics = None
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.model.to(device)
+        self.wandb_log()
+        self.start_time = None
+
+    def train(self):
+
+        self.history.clear()
+        loss = 0
+        val_loss = 0
+
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        
+
+        for epoch in range(self.epochs):
+
+            epoch_loss = []
+            epoch_val_loss = []
+            epoch_metrics = []
+            items = len(self.dataloader['train'])
+            self.start_time = time.time()
+
+            self.model.train()
+            item = 1
+            for batch in self.dataloader['train']:
+                self.optimizer.zero_grad()
+                data = batch[0].to(device)
+                preds = self.model(data)
+                del data
+                target = batch[1].to(device)
+                loss = self.criterion(preds, target)
+                loss.backward()
+                del target
+                del preds
+                epoch_loss.append(loss.numpy(force=True))
+                self.optimizer.step()
+                self.print_log(epoch_log=False, epoch=epoch, item=item, items=items, train_loss=loss)
+                item += 1
+
+            items = len(self.dataloader['test'])
+            self.model.eval()
+            item = 1
+            with torch.no_grad():
+                for batch in self.dataloader['test']:
+                    data = batch[0].to(device)
+                    preds = self.model(data)
+                    del data
+                    target = batch[1].to(device)
+                    val_loss = self.criterion(preds, target)
+                    if self.metrics:
+                        metric = self.metrics(preds, target)
+                        epoch_metrics.append(metric.numpy(force=True))
+                    del target
+                    del preds
+                    epoch_val_loss.append(val_loss.numpy(force=True))
+                    self.print_log(epoch_log=False, epoch=epoch, item=item, items=items, train_loss=loss,
+                                   val_loss=val_loss)
+                    item += 1
+
+            loss = np.array(epoch_loss).mean()
+            val_loss = np.array(epoch_val_loss).mean()
+            metric = np.array(epoch_metrics).mean() if self.metrics else 0
+
+            if self.scheduler:
+                self.scheduler.step(loss)
+
+            self.history.loss.append(loss)
+            self.history.val_loss.append(val_loss)
+            self.history.metric.append(metric)
+
+            self.print_log(epoch_log=True, epoch=epoch, item=item, items=items, train_loss=loss, val_loss=val_loss,
+                           metric=metric)
+
+        return self.history
+
+    def set_metrics(self, metric_name, metric_function):
+        self.metric_name = metric_name
+        self.metrics = metric_function
+
+    def print_log(self, epoch_log=True, epoch=0, item=0, items=0, train_loss=0, val_loss=0, metric=0):
+        lr = self.optimizer.param_groups[0]["lr"]
+        log = {'epoch': epoch + 1,
+               'epoch/epochs': str(epoch + 1) + '/' + str(self.epochs),
+               'train/loss': train_loss,
+               'val/loss': val_loss}
+        if epoch_log:
+            log['lr'] = lr
+            if self.metrics:
+                metric_key = f'metric/{self.metric_name}'
+                log[metric_key] = metric
+
+        if epoch_log:
+            log_str = f'epoch {epoch + 1}/{self.epochs}... '
+        else:
+            log_str = f'epoch {epoch + 1}/{self.epochs}... batch {item}/{items} '
+
+        for item in [item for item in log if item != 'epoch' and item != 'epoch/epochs' and item != 'item']:
+            if torch.is_tensor(log[item]):
+                value = log[item].item()
+                value = f'{value:.4f}'
+            else:
+                if type(log[item]) == np.float32:
+                    value = f'{log[item]:.4f}'
+                else:
+                    value = log[item]
+            log_str = log_str + ' ' + item + ': ' + str(value)
+        dt = int(time.time() - self.start_time)
+        log_str_time = f'; time:{dt//3600:03d}:{(dt//60)%60:02d}:{dt%60:02d}'
+        log_str = log_str + log_str_time
+
+        if epoch_log:
+            print(log_str, end='\n')
+        else:
+            print(log_str, end='\r')
+
+        if self.wandb and epoch_log:
+            wandb.log(log, step=epoch)
+
+    def wandb_log(self, wandb=False):
+        self.wandb = wandb
+
+
+class train_history():
+    loss = []
+    val_loss = []
+    metric = []
+
+    def init(self):
+        self.loss = []
+        self.val_loss = []
+        self.metric = []
+
+    def clear(self):
+        self.loss.clear()
+        self.val_loss.clear()
+        self.metric.clear()
+
+    def plot(self, train=True, val=True, metric=None):
+        if train:
+            plt.plot(self.loss, label='train loss')
+        if val:
+            plt.plot(self.val_loss, label='val loss')
+        if metric:
+            plt.plot(self.metric, label=metric)
+        plt.legend()
+        plt.show()       
